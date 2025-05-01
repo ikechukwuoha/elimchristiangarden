e0e43a7a17428e286b176596f6aef9f295b748c1
Ikechukwu Ohanugodu <ikechukwuandrew4@gmail.com>
2025-05-01 18:46:09 +0100

    sermon detailed page fixed56

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -133,7 +133,7 @@ export default function Footer() {
                 <svg className="h-5 w-5 mr-3 text-green-500 mt-1 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                 </svg>
-                <span className="text-gray-300">123 Church Street,<br />City, State 12345</span>
+                <span className="text-gray-300">Elim Garden Kilometer 2,<br />Kuduru Express Way Bwari Abuja</span>
               </div>
               <div className="flex items-center">
                 <svg className="h-5 w-5 mr-3 text-green-500 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
